169bff64d48cfd7cb512cb389d57a7bb535c0982
Aidan Saavedra Buckley <asaavedrabuckley@gmail.com>
2019-11-05 03:16:42 -0800

    finish up graphs

--- README.md
+++ README.md
@@ -1,26 +1,6 @@
 # Scaling Operations Modeling
 
-## How does the business work?
-* The amount of waste to pick up is driven by the number of customers (lever) and the waste per customer (non-lever)
-* This translates to a number of lifts that need to be performed
-  * Number of lifts to perform translates to a number of shifts -> this is dictated by the business's efficiency
-    * Lower LPS means more trucks, more labor, more fuel cost, etc. 
-    * Performing these lifts at the lowest possible cost is the key operation for the business
-* Once the lifts have been performed, they are disposed of, which incurs another cost based on the weight of the waste
-
-
-* Revenue = Lifts * $/Lift
-  * Lifts = lifts/shift * shifts
-    * Shifts = days worked * shifts_per_day
-    * lifts/shift = ?
-  * $/Lift = $/volume * volume/lift
-    * $/volume = ?
-    * volume/lift = fixed
-
-* Cost to serve = lifts * cost/lift
-  * Lifts = lifts/shift * shifts
-    * Shifts = days worked * shifts_per_day
-    * lifts/shift = ?
-  * cost/lift = driver_labor/lift + other_labor/lift + disposal/lift + fuel/lift + maintenance/lift + depreciation/lift
-    * 
- 
+* This repo contains the code for the model used in the exam
+* The main logic of the model is contained in `model.py` 
+* The capacity decision modeling is in `capacity_decision.py`
+* Graphs are created in `analysis.py`
--- src/analysis.py
+++ src/analysis.py
@@ -15,6 +15,8 @@ def save_chart(chart: alt.Chart, title: str) -> None:
 
 def gen_data() -> pd.DataFrame:
     model = Model()
+    # assume their performance slips
+    model.inputs.lifts_per_truck_day = 10
     current_customers = model.inputs.num_customers
     customers_3x = current_customers * 3
     n_customers = range(current_customers, customers_3x, 500)
@@ -38,7 +40,62 @@ def make_line_chart(df, x, y, title) -> alt.Chart:
     return chart
 
 
+def gen_heatmap_data() -> pd.DataFrame:
+    model = Model()
+    curr_lpt = int(model.inputs.lifts_per_truck_day * 1000)
+    curr_price = int(model.inputs.revenue_per_m3)
+    lpt_range = range(curr_lpt - 1000, curr_lpt + 1001, 100)
+    price_range = range(curr_price - 5, curr_price + 6, 1)
+    roics = []
+    prices = []
+    lpts = []
+    for l in lpt_range:
+        for p in price_range:
+            model.inputs.lifts_per_truck_day = l / 1000
+            model.inputs.revenue_per_m3 = p
+            model.set_trucks()
+            r = model.new_roic()
+            roics.append(r)
+            prices.append(p)
+            lpts.append(l / 1000)
+    df = pd.DataFrame(
+        {"roic": roics, "price_per_m3": prices, "lifts_per_truck": lpts}
+    ).assign(roic_str=lambda x: x["roic"].apply(lambda y: "{:.1%}".format(y)))
+    return df
+
+
+def make_heatmap(df) -> alt.Chart:
+    chart = (
+        alt.Chart(df)
+        .mark_rect()
+        .encode(x="lifts_per_truck:O", y="price_per_m3:O", color="roic")
+        .properties(title="ROIC Sensitivity to Lifts per Truck and Price per m3")
+    )
+
+    text = (
+        alt.Chart(df)
+        .mark_text(baseline="middle", fontSize=9)
+        .encode(text="roic_str", x="lifts_per_truck:O", y="price_per_m3:O")
+    )
+
+    return (chart + text).properties(width=650, height=400)
+
+
+def make_disposal_data() -> pd.DataFrame:
+    model = Model()
+    cost_per_tonne = model.cost_per_tonne()
+
+
 if __name__ == "__main__":
     df = gen_data()
-    chart = make_line_chart(df, "customers", "ROIC", "ROIC and Customer Growth")
-    save_chart(chart, "ROIC_and_growth")
+    chart = make_line_chart(
+        df,
+        "customers",
+        "ROIC",
+        "ROIC and Customer Growth with Efficiency Deterioration",
+    )
+    save_chart(chart, "ROIC_and_growth - deteriorate")
+
+    # hm_df = gen_heatmap_data()
+    # heatmap = make_heatmap(hm_df)
+    # save_chart(heatmap, "Heatmap.png")
